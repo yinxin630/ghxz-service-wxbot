e00eb5234151d133c5c009e7319d7ba7b3cb5f20
碎碎酱 <yinxin630@gmail.com>
2016-02-02 12:16:39 +0800

    feat(preload): add log in code

--- preload.js
+++ preload.js
@@ -16,5 +16,33 @@ function sleep(duration) {
 }
 
 CO(function* () {
-    
+    var hasLogged = false;
+    while (!hasLogged) {
+        hasLogged = checkLoginStatus();
+        yield sleep(500);
+    }
+    NativeConsole.log('登录完毕');
 });
+
+function checkLoginStatus () {
+    var qrcodeDiv = document.querySelector('.qrcode');
+    var chatItemDiv = document.querySelector('.chat_item')
+
+    if (chatItemDiv) {
+        NativeConsole.log('用户已登录');
+        return true;
+    }
+    else if (qrcodeDiv) {
+        var qrcodeImage = qrcodeDiv.firstElementChild;
+        if (qrcodeImage && qrcodeImage.src.match(/.*login.weixin.qq.com\/qrcode\/.*/)) {
+            NativeConsole.log(`获取到二维码 -> [${qrcodeDiv.firstElementChild.src}] 请扫一扫登录`);
+        }
+        else {
+            NativeConsole.log('用户未登录，获取登录二维码');
+        }
+    }
+    else {
+        NativeConsole.log('页面未加载完毕');
+    }
+    return false;
+}
